a8b6cf7df5fec250b6b00e4b587daf0b1b0c13e6
alvaro <alvaro@forak.mx>
2026-03-06 21:06:26 -0600

    Cambios e calidad en servidor

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    emptyOutDir: true
+  },
+  // Importante para SPA
+  preview: {
+    port: 3000,
+    host: true
+  }
 })
